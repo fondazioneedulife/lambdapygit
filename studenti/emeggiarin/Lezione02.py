843dad7d50f5e3e216a3f6c2f7ffac9ef47c3bab
emanuele23dev <emanuele.meggiarin@libero.it>
2025-11-03 17:44:26 +0100

    lezione 02

---
+++ studenti/emeggiarin/Lezione02.py
@@ -0,0 +1,31 @@
+
+
+while True:
+   try:
+      num1 = float(input("Inseirsci numero 1: "))
+      num2 = float(input("Inserisci numero 2: "))
+   except ValueError:
+       print("Input non valido. Per favore inserisci dei numeri.")
+   operazione = input("Inserisci operazione (+,-,*,/): ")
+
+   if operazione == "+":
+      risultato = num1 + num2
+      print(risultato)
+   elif operazione == "-":
+      risultato = num1 - num2
+      print(risultato)
+   elif operazione == "*":
+      risultato = num1 * num2
+      print(risultato)
+   elif operazione == "/":
+      risultato = num1 / num2
+      print(risultato)
+   else:
+      risultato = "Operazione non valida"
+      print(risultato)
+
+   continua = input("Vuoi fare un'altra operazione? (s/n): ")
+
+   if continua.lower() != "s":
+      print("Programma finito.")
+      break
