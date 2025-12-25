4ba77469d83839a3953b39accfeb0b26c5029c3d
Matteo Lovato <matteo.lovato@allievi.itsdigitalacademy.com>
2025-12-25 22:36:04 +0100

    file api aggiunto owner e descrizione sull'hover e link alla repo sulle label

--- studenti/mlovato/api/python_repos_visual.py
+++ studenti/mlovato/api/python_repos_visual.py
@@ -15,16 +15,24 @@ response_dict = r.json()
 
 #esplora le info sui repository
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_links, stars, hover_texts = [], [], []
 
 # Esamina il primo repository
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
+    # costruisco i testi sul mouse hover
+    owner = repo_dict['owner']['login'] # nome login dell'owner
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
 
 # crea la visualizzazione
 title = "Most-Starred Python Projects on GitHub"
 labels = { "x": "Repository", "y": "Stars"}
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels)
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hover_texts)
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
 fig.show()
