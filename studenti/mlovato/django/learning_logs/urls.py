407e2074d6cc9183bc74291796388eca24280f18
Matteo Lovato <matteo.lovato@allievi.itsdigitalacademy.com>
2026-01-10 13:39:30 +0100

    aggiunte viste per topics e topic

--- studenti/mlovato/django/learning_logs/urls.py
+++ studenti/mlovato/django/learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = "learning_logs"
 urlpatterns = [ 
     #Home Page
     path("", views.index, name="index"),
+    # Pagina delle materie
+    path("topics/", views.topics, name="topics"),
+    # Pagina degli argomenti per ogni materia
+    path("topics/<int:topic_id>/", views.topic, name="topic")
     ]
